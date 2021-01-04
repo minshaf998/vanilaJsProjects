69e52253ed4f20d0b9c61f7733ad77d05b18dda3
minshaf998 <minshaf998@gmail.com>
2021-01-04 17:36:26 +0530

    change bg color another way

--- changeBackgroudColor/script.js
+++ changeBackgroudColor/script.js
@@ -1,11 +1,26 @@
-const button = document.querySelector("button");
+const button_green = document.getElementById("green");
 const body = document.querySelector("body");
 const colors = ["red", "green", "blue", "yellow", "pink", "purple", "grey"];
 
-body.style.backgroundColor = "violet";
-button.addEventListener("click", changeBackground);
+const button_red = document.getElementById("blue");
+
+body.style.backgroundColor = "#12AB";
+button_green.addEventListener("click", changeBackground);
 
 function changeBackground() {
     const colorIndex = parseInt(Math.random() * colors.length);
     body.style.backgroundColor = colors[colorIndex];
+}
+
+button_red.addEventListener("click",random);
+
+const colvals = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
+
+
+function random(){
+    var str = '#';
+    for(var i=0;i<4;i++){
+        str+= colvals[Math.floor(Math.random()*colvals.length)];
+    }
+    body.style.backgroundColor = str;
 }
