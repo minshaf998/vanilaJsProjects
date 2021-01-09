1b2523bdd7923f8ea121306d4eff2cf119760632
minshaf998 <minshaf998@gmail.com>
2021-01-09 13:02:52 +0530

    organized

--- changeBackgroudColor/script.js
+++ changeBackgroudColor/script.js
@@ -1,36 +1,46 @@
-const button_green = document.getElementById("green");
+//======================================first button
 const body = document.querySelector("body");
-const colors = ["red", "green", "blue", "yellow", "pink", "purple", "grey"];
+const colors = ["red", "green", "blue", "yellow", "pink", "purple", "grey"]; // initiating colors
 
-const button_blue = document.getElementById("blue");
-
-body.style.backgroundColor = "#12AB";
-button_green.addEventListener("click", changeBackground);
+body.style.backgroundColor =
+    colors[parseInt(Math.floor(Math.random() * colors.length))]; //starting random background color
 
 function changeBackground() {
-    const colorIndex = parseInt(Math.random() * colors.length);
+    const colorIndex = parseInt(Math.floor(Math.random() * colors.length));
     body.style.backgroundColor = colors[colorIndex];
-    button_green.innerHTML = colors[colorIndex];
 }
 
-button_blue.addEventListener("click",random);
+//========================================second button
 
-const colvals = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
+const colvals = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+];
 
-
-function random(){
-    var str = '#';
-    for(var i=0;i<4;i++){
-        str+= colvals[Math.floor(Math.random()*colvals.length)];
-        button_blue.innerHTML = str;
+function random() {
+    var str = "#";
+    for (var i = 0; i < 4; i++) {
+        str += colvals[Math.floor(Math.random() * colvals.length)];
     }
     body.style.backgroundColor = str;
 }
 
-const button_red = document.getElementById('red');
-button_red.addEventListener('click',blink);
-
-function blink(){
+//==============================================third button
+function blink() {
     random();
-    setTimeout(blink,500);
+    setTimeout(blink, 1000);
 }
